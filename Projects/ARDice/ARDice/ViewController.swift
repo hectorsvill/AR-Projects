102d8ef80194556cc3df2a126e7dd883d878b48c
hectorsvill <hectorstevenvillasano@gmail.com>
2020-09-22 12:19:53 -0700

    init

---
+++ Projects/ARDice/ARDice/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  ARDice
+//
+//  Created by Hector Villasano on 9/22/20.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        sceneView.delegate = self
+        
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        sceneView.scene = scene
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if ARWorldTrackingConfiguration.isSupported {
+            let configuration = ARWorldTrackingConfiguration()
+            sceneView.session.run(configuration)
+            
+        } else {
+            print("ARWorldTrackingConfiguration not supported")
+        }
+
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        sceneView.session.pause()
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # AR-Projects
 Learning and experimenting with Augmented Reality 
+
+[ARKit](https://developer.apple.com/augmented-reality/arkit/)
+
+#
